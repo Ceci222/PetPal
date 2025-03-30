dd371fbfe623f7d46cc256f67b7a428a2b7d36f3
Cecilia <ceci_2504@hotmail.com>
2025-03-30 19:07:52 +0200

    Add dynamic contact form and event listener - Update navigation links

--- js/index.js
+++ js/index.js
@@ -1,11 +1,97 @@
-/* 
+
+
+
+document.getElementById('contact-link').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    showContactForm(); 
+});
+
 
 function showContactForm(){
     const contactSection = document.getElementById('contact');
 
+    //Campo name
+
     const contactH1 = document.createElement('h1');
 
-    const label = document.createElement('p');
+    const labelName = document.createElement('label');
+
+    labelName.setAttribute('for','name'); // estoy diciendo que el for y el id ambos seran name y estaran realcionados
+
+    labelName.textContent = 'Name'; //lo que verá el usuario en la etiqueta
+
+    const inputName =  document.createElement('input');
+
+    inputName.setAttribute('type', 'text') //debo determinar como primer arg que me refiero al tipo y luego el segundo indicará que es tipo texto, podría ser email, password...
+
+    inputName.setAttribute('id', 'name') //establezco que el id sera name
+
+    inputName.setAttribute('placeholder', 'Your name');
+
+    inputName.setAttribute('required', true); //el true se puede reemplazar por comillas vacías o repetir "required"
+
+    
+
+    // Campo email
+    const labelEmail = document.createElement('label');
+
+    labelEmail.setAttribute('for', 'email');
+
+    labelEmail.textContent = 'Email';
 
 
-} */
+    const inputEmail = document.createElement('input');
+
+    inputEmail.setAttribute('type', 'email');
+
+    inputEmail.setAttribute('id', 'email');
+
+    inputEmail.setAttribute('placeholder', 'Your Email');
+
+    inputEmail.setAttribute('required', true);
+
+
+    // Textarea
+
+   /*  <label for="message">Message</label>
+    <textarea name="message" id="message" required>Message</textarea> */
+
+    const textareaLabel = document.createElement('label');
+
+    textareaLabel.setAttribute('for', 'message');
+
+    textareaLabel.textContent = 'Message';
+
+
+    const textareaInput = document.createElement('textarea');
+    
+    textareaInput.setAttribute('name', 'message');
+
+    textareaInput.setAttribute('id', 'message');
+
+    textareaInput.setAttribute('placeholder', 'Message')
+
+    textareaInput.setAttribute('required', true);
+
+    //Botón enviar
+
+    // <button class="submit button" type="submit"> Submit </button>
+
+    const submitButton = document.createElement('button');
+
+    submitButton.setAttribute('type', 'submit');
+
+    submitButton.setAttribute('class', 'submit button'); //debo añadir ambas clases en un mismo atributo o usar submitButton.classList.add('submit', 'button');
+
+    //Añadir los elementos creados, los puedo agregar a medida que se crean o al final todos juntos
+
+    contactSection.appendChild(contactH1);
+    contactSection.appendChild(labelName);
+    contactSection.appendChild(inputName);
+    contactSection.appendChild(labelEmail);
+    contactSection.appendChild(inputEmail);
+    contactSection.appendChild(textareaLabel);
+    contactSection.appendChild(textareaInput);
+    contactSection.appendChild(submitButton);
+
+} 
